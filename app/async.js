7d3c3bcd506042d4bd103613882c12e3dee3affe
Dennis Onder <webdev.dennisonder@gmail.com>
2019-05-29 01:36:56 +0200

    Async behaviour complete

--- app/async.js
+++ app/async.js
@@ -8,6 +8,21 @@ define(["jquery"], function($) {
       return Promise.resolve(value);
     },
 
-    manipulateRemoteData: function(url) {}
+    manipulateRemoteData: function(url) {
+      return new Promise((resolve, reject) => {
+        $.ajax({
+          type: "GET",
+          url,
+          dataType: "json",
+          success: function(response) {
+            let result = [];
+            for (let [key, value] of Object.entries(response.people)) {
+              result.push(value.name);
+            }
+            resolve(result.sort());
+          }
+        });
+      });
+    }
   };
 });
